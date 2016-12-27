d9bb35ab4e54939d2bb069c438cd67864bdae7e8
marsstar333 <mars@Marss-iMac.local>
2016-12-27 21:25:19 +0100

    Post merger version showing problem with Azure.Mobile.Client

---
+++ PacificCoral/Droid/NetworkConnection.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Content;
+using Android.Net;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PacificCoral.Droid;
+using PacificCoral.Helpers;
+
+
+using Xamarin.Forms;
+using Application = Android.App.Application;
+
+[assembly: Dependency(typeof(PacificCoral.Droid.NetworkConnection))]
+namespace PacificCoral.Droid
+{
+    public class NetworkConnection : INetworkConnection
+    {
+        public bool IsConnected { get; set; }
+        public bool IsOnline { get; set; }
+
+        public void CheckNetworkConnection()
+        {
+            var connectivityManager = (ConnectivityManager)Application.Context.GetSystemService(Context.ConnectivityService);
+            var activeNetworkInfo = connectivityManager.ActiveNetworkInfo;
+            if (activeNetworkInfo != null && activeNetworkInfo.IsConnectedOrConnecting)
+            {
+                IsConnected = true;
+                IsOnline = isOnline();
+            }
+            else
+            {
+                IsConnected = false;
+                IsOnline = false;
+            }
+        }
+        private bool isOnline()
+        {
+
+            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+            try
+            {
+                System.Net.NetworkInformation.PingReply pr = p.Send("8.8.8.8", 128);
+                if (pr != null && pr.Status == System.Net.NetworkInformation.IPStatus.Success)
+                    return true;
+                else
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- PacificCoral/PacificCoral/ViewModels/SignInViewModel.cs
+++ PacificCoral/PacificCoral/ViewModels/SignInViewModel.cs
@@ -11,6 +11,8 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using PropertyChanged;
 using Xamarin.Forms;
+using PacificCoral.Helpers;
+using Acr.UserDialogs;
 
 namespace PacificCoral.ViewModels
 {
@@ -18,6 +20,8 @@ namespace PacificCoral.ViewModels
     public class SignInViewModel : BindableBase
     {
         private readonly INavigationService _navigationService;
+        private bool isOnline = false;
+        private bool waitVisible = true;
 
         public SignInViewModel() { }
 
@@ -30,10 +34,78 @@ namespace PacificCoral.ViewModels
         {
             get
             {
+
                 return new DelegateCommand(async () =>
                 {
-                    await _navigationService.NavigateAsync<DashBoardView>();
+                    await LoginAsync();
                 });
+            }
+        }
+
+        private async Task LoginAsync()
+        {
+            // check if phone is online
+            NetworkConnection.DefaultConnection.CheckNetworkConnection();
+            if (NetworkConnection.DefaultConnection.IsOnline)
+            {
+                if (Authentication.DefaultAthenticator != null)
+                {
+                    WaitVisible = true;
+                    await Authentication.DefaultAthenticator.Authenticator.Authenticate();
+                    WaitVisible = false;
+                }
+                if (Authentication.DefaultAthenticator.IsAuthenticated)
+                {
+                    await _navigationService.NavigateAsync<DashBoardView>();
+                }
+                else // authentication failure
+                {
+                    if (Authentication.DefaultAthenticator.UserInfo == null)
+                    {
+                        // Did not authenticate with azure
+                        UserDialogs.Instance.Alert ("Login Failure.  Please try again.", "Login Failure");
+
+                    }
+                    else
+                    {
+                        // unable to login client
+                        Settings.LastLoggedinUser = Authentication.DefaultAthenticator.UserInfo.DisplayableId;
+                        UserDialogs.Instance.Alert("WARNING:  Failure logging in application to server.  Application will run in OFFLINE mode with limitted functionality.", "Login Problems");
+                        await _navigationService.NavigateAsync<DashBoardView>();
+                    }
+
+                }
+            }
+            else // offline
+            {
+                // If has previous login, use in offline mode
+                if (Settings.LastLoggedinUser!=string.Empty)
+                {
+                    UserDialogs.Instance.Alert("WARNING:  Failure logging in application to server.  Application will run in OFFLINE mode with limitted functionality.", "Conneciton Error");
+                    await _navigationService.NavigateAsync<DashBoardView>();
+                }
+                else
+                {
+                    // no default user, so cannot use in offline mode.
+                    UserDialogs.Instance.Alert ("The phone is not connected to the internet and is unable to log into the server.  Check Wifi and cellular connection, and try again.","Connection Error");
+                }
+            }
+
+
+        }
+
+
+        public bool WaitVisible
+        {
+            get
+            {
+                return waitVisible;
+            }
+
+            set
+            {
+                SetProperty<bool>(ref waitVisible, value);
+                
             }
         }
     }
