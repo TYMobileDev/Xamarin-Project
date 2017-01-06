32fea6ab2060fa2295586f69aa9f22ac3c0d1f16
Anastasia Mizumskaya <anastasia.mizumskaya@headworks.com.ua>
2017-01-06 16:13:31 +0200

    clean

--- PacificCoral/PacificCoral/Views/RootPage.cs
+++ PacificCoral/PacificCoral/Views/RootPage.cs
@@ -8,10 +8,6 @@ namespace PacificCoral
 {
 	public class RootPage : TabbedPage, INavigationAware
 	{
-		//private Page ProfileView = new NavigationPage(new ProfileView()) { Icon ="accounttabbar", Title = "1" };
-		//private Page DashBoardView = new NavigationPage(new DashBoardView()) { Icon = "dashboardtabbar", Title = "2" };
-		//private Page InventoryView = new NavigationPage(new InventoryView()) { Icon = "itemstabbar", Title = "3" };
-
 		private Page ProfileView = new ProfileView() { Icon = "accounttabbar", Title = " " };
 		private Page DashBoardView = new DashBoardView() { Icon = "dashboardtabbar", Title = "  " };
 		private Page InventoryView = new InventoryView() { Icon = "itemstabbar", Title = "   " };
@@ -30,12 +26,10 @@ namespace PacificCoral
 
 		public void OnNavigatedFrom(NavigationParameters parameters)
 		{
-
 		}
 
 		public void OnNavigatingTo(NavigationParameters parameters)
 		{
-
 		}
 
 		public void OnNavigatedTo(NavigationParameters parameters)
